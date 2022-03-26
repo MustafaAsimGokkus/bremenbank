560a18c6c1c8b05c154cb38ceec8aa4f1918d0cf
Asim Gokkus <asimgokkus@gmail.com>
2022-03-26 14:59:00 +0100

    day 7.1.5337

--- eclipse-workspace/surebank/src/main/java/com/bank/sure/config/WebSecurityConfig.java
+++ eclipse-workspace/surebank/src/main/java/com/bank/sure/config/WebSecurityConfig.java
@@ -1,14 +1,22 @@
 package com.bank.sure.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.bank.sure.security.AuthTokenFilter;
+import com.bank.sure.security.service.UserDetailsServiceImpl;
 
 import lombok.AllArgsConstructor;
+import net.bytebuddy.implementation.bind.annotation.Super;
 
 
 @Configuration
@@ -17,9 +25,27 @@ import lombok.AllArgsConstructor;
 @EnableGlobalMethodSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
+	private UserDetailsServiceImpl userDetailsServiceImpl;
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthTokenFilter authenticationJwtTokeFilter() {
+		return new AuthTokenFilter();
+	}
+	
+	@Bean
+	protected  AuthenticationManager authenticationManager()throws Exception{
+		return super.authenticationManager();
+	}
+	
+	//we connect our class with the security structure
 	@Override
 		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-			super.configure(auth);
+			auth.userDetailsService(userDetailsServiceImpl);
 		}
 	
 	@Override
--- eclipse-workspace/surebank/src/main/java/com/bank/sure/security/JwtUtils.java
+++ eclipse-workspace/surebank/src/main/java/com/bank/sure/security/JwtUtils.java
@@ -2,19 +2,29 @@ package com.bank.sure.security;
 
 import java.util.Date;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+
 import org.hibernate.sql.ordering.antlr.GeneratedOrderByFragmentRendererTokenTypes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import com.bank.sure.security.service.UserDetailsImpl;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 
 @Component
 public class JwtUtils {
-    @Value("${surebank.app.jwtSecret}")
+   
+	private static final Logger LOGGER = LoggerFactory.getLogger(JwtUtils.class);
+	
+	@Value("${surebank.app.jwtSecret}")
     private String jwtSecret;
     @Value("${surebank.app.jwtExpirationMs}")
     private long jwtExpirationMs;
@@ -22,13 +32,39 @@ public class JwtUtils {
     public String generateToken (Authentication authentication) {
 		UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 	
-		return Jwts.builder().
-				setSubject(userPrincipal.getUsername()).
-				setIssuedAt(new Date()).
-				setExpiration(new Date((new Date()).getTime()+jwtExpirationMs).
-			    signWith(SignatureAlgorithm.HS512, jwtSecret).
-			    compact();
-	
+		return Jwts.builder()
+				.setSubject(userPrincipal.getUsername())
+				.setIssuedAt(new Date())
+				.setExpiration(new Date((new Date()).getTime()+jwtExpirationMs))
+			    .signWith(SignatureAlgorithm.HS512, jwtSecret)
+			    .compact();
 	}
 	
+    public boolean validateToken (String token) {
+    	
+    	try {
+    	Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+    	return true;
+    	}catch (ExpiredJwtException e) {
+    		LOGGER.error("JWT Token expired{}",e.getMessage());
+    	}catch (MalformedJwtException e) {
+    		LOGGER.error("JWT Token Malformed{}",e.getMessage());
+    	}catch (UnsupportedJwtException e) {
+    		LOGGER.error("JWT Token unsupported{}",e.getMessage());
+    	}catch ( SignatureException e) {
+    		LOGGER.error("Invalid JWT signature{}",e.getMessage());
+    	}catch (IllegalArgumentException e) {
+    		LOGGER.error("JWT Token illegal argument exception {}",e.getMessage());
+    	}
+    	return false;
+    }
+    
+    public String getUserNameFromJwtToken(String token) {
+    	return Jwts.parser()
+    			.setSigningKey(jwtSecret)
+    			.parseClaimsJws(token)
+    			.getBody()
+    			.getSubject();
+    }
+    
 }
